1497b05a8c941ebd271e544aa25d678d4d3dd9ad
zmahfuz061318 <str053mgr@gmail.com>
2020-07-21 22:02:53 -0400

    Work in progress

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import './Timeline.css';
+import Status from './Status'
+
+
+
 
 function Timeline() {
   return (
     <div className="timeline">
-      <div className="status">
-        <div className="user-info">
-          <img src="https://ga-core.s3.amazonaws.com/cms/files/files/000/000/886/original/ga-logo-gear.png" alt="General Assembly Logo" className="profile-pic" />
-          <div className="user-text">
-            <p><a href="">General Assembly</a></p>
-            <p><small>April 3 at 10:25 AM</small></p>
-          </div>
-        </div>
-        <p className="status-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure ducimus beatae, eveniet magnam quam aperiam harum voluptatem, minus debitis repudiandae saepe. Enim, veritatis tempore possimus ex harum ad saepe inventore?
-        </p>
-        <hr/>
-        <div className="reaction-bar">
-          <button>Like</button>
-          <button>Comment</button>
-          <button>Share</button>
-        </div>
-      </div>
+
+
+
+      <Status status = "user-info" />
+      <Status />
+      <Status />
+
+
     </div>
+
+    
   );
 }
 
